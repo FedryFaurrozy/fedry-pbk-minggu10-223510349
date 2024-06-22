6ee7e2688c130c1abb1000df0138fab6ab6bf8f8
Fedry Faurrozy <istanap303@gmail.com>
2024-06-23 02:53:04 +0700

    FINAL TUGAS AKHIR PBK SEMS 5

--- src/App.vue
+++ src/App.vue
@@ -16,33 +16,6 @@
         </ul>
       </nav>
     </header>
-    <div v-if="todoListVisible" id="todoList">
-      <input
-        type="text"
-        v-model="newTodo"
-        placeholder="Tambahkan To Do baru"
-        class="input-todo"
-      />
-      <button @click="addTodo" class="btn-todo-add">Tambah</button>
-      <ul id="todoItems">
-        <li v-for="item in todos" :key="item.id">
-          {{ item.text }}
-          <button @click="removeTodo(item.id)">Hapus</button>
-          <input type="checkbox" @change="toggleTodo(item.id)" />
-        </li>
-      </ul>
-      <h3>Tugas Selesai:</h3>
-      <ul id="completedTasks">
-        <li
-          v-for="item in completedTodos"
-          :key="item.id"
-          class="completed-task"
-        >
-          {{ item.text }}
-          <button @click="removeCompletedTodo(item.id)">Hapus</button>
-        </li>
-      </ul>
-    </div>
     <div v-if="postListVisible" id="postList">
       <select v-model="selectedUser" @change="fetchPosts" class="user-select">
         <option v-for="user in users" :value="user.id" :key="user.id">
@@ -55,6 +28,17 @@
         </li>
       </ul>
     </div>
+    <div v-if="todoListVisible" id="todoList">
+      <todo-list />
+      <div class="todo-explanation">
+        <p>
+          Klik <strong>Semua</strong> untuk melihat semua kegiatan dan klik
+          <strong>Belum Selesai</strong> untuk dapat menceklis atau
+          menyelesaikan tugas. Manfaatkan fitur ini untuk mengatur kegiatan
+          harianmu dengan lebih efektif!
+        </p>
+      </div>
+    </div>
     <footer>
       <p>Dikembangkan oleh fedry faurrozy</p>
     </footer>
@@ -62,28 +46,30 @@
 </template>
 
 <script>
+import TodoList from "./TodoList.vue";
+
 export default {
+  components: {
+    TodoList,
+  },
   data() {
     return {
-      todos: [],
-      completedTodos: [],
-      newTodo: "",
       posts: [],
       users: [],
       selectedUser: null,
-      todoListVisible: false,
       postListVisible: false,
+      todoListVisible: false,
     };
   },
   methods: {
-    showTodos() {
-      this.todoListVisible = true;
-      this.postListVisible = false;
-    },
     showPosts() {
       this.fetchUsers();
       this.postListVisible = true;
       this.todoListVisible = false;
+    },
+    showTodos() {
+      this.todoListVisible = true;
+      this.postListVisible = false;
     },
     fetchUsers() {
       fetch("https://jsonplaceholder.typicode.com/users")
@@ -103,103 +89,10 @@ export default {
           });
       }
     },
-    addTodo() {
-      if (this.newTodo) {
-        this.todos.push({
-          id: Date.now(),
-          text: this.newTodo,
-          completed: false,
-        });
-        this.newTodo = "";
-      }
-    },
-    removeTodo(id) {
-      this.todos = this.todos.filter((todo) => todo.id !== id);
-    },
-    removeCompletedTodo(id) {
-      this.completedTodos = this.completedTodos.filter(
-        (todo) => todo.id !== id
-      );
-    },
-    toggleTodo(id) {
-      const todo = this.todos.find((todo) => todo.id === id);
-      todo.completed = !todo.completed;
-      if (todo.completed) {
-        this.completedTodos.push(todo);
-        this.removeTodo(id);
-      } else {
-        this.todos.push(todo);
-        this.completedTodos = this.completedTodos.filter(
-          (todo) => todo.id !== id
-        );
-      }
-    },
   },
 };
 </script>
 
 <style scoped>
-body {
-  font-family: Arial, sans-serif;
-  margin: 0;
-  padding: 0;
-}
-header {
-  background-color: #343a40;
-  padding: 10px 0;
-  color: #f8f9fa;
-  text-align: center;
-}
-nav ul {
-  list-style: none;
-  padding: 0;
-  margin: 0;
-  display: flex;
-  justify-content: center;
-}
-nav ul li {
-  margin: 0 15px;
-}
-nav ul li a {
-  color: #f8f9fa;
-  text-decoration: none;
-  font-weight: bold;
-  padding: 10px 15px;
-  border-radius: 5px;
-  transition: background-color 0.3s;
-}
-nav ul li a:hover {
-  background-color: #007bff;
-}
-.input-todo,
-.user-select {
-  padding: 10px;
-  margin-bottom: 10px;
-  width: 80%;
-  border-radius: 5px;
-  border: 1px solid #ccc;
-}
-.btn-todo-add {
-  padding: 10px 20px;
-  background-color: #007bff;
-  color: white;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-}
-.completed-task {
-  text-decoration: line-through;
-}
-footer {
-  background-color: #343a40;
-  color: #f8f9fa;
-  text-align: center;
-  padding: 20px 0;
-  position: absolute;
-  bottom: 0;
-  width: 100%;
-}
-footer p {
-  margin: 0;
-}
+@import "./style.css";
 </style>
